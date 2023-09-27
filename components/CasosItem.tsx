3b1aa2d4d3d52fdba430a3376a8185e824d53a50
pews-dev <pedro.silvaifce@gmail.com>
2023-09-26 22:04:32 -0300

    feat: mudando layout do card de eventos

--- components/CasosItem.tsx
+++ components/CasosItem.tsx
@@ -1,190 +1,161 @@
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { View, Image, Text, Linking } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { View, Image, Text, Linking } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import heartOutlineIcon from './icons/heart-outline.png';
-import unfavoriteIcon from './icons/unfavorite.png';
-import whatsappIcon from './icons/whatsapp.png';
+import heartOutlineIcon from "./icons/heart-outline.png";
+import unfavoriteIcon from "./icons/unfavorite.png";
+import whatsappIcon from "./icons/whatsapp.png";
+import { useNavigation } from '@react-navigation/native';
 
+type TCases = {
+  id: string;
+  avatar: string;
+  name: string;
+  subject: string;
+  bio: string;
+  cost: string;
+}
 
-export default function CasosItem({ teacher: casos, favorited }) {
-    const [isFavorited, setIsFavorited] = useState(favorited);
+type TCaseProps = {
+  casos: TCases;
+  favorited: any;
+};
 
-    /*function handleLinkToWhatsapp() {
-      api.post('connections', {
-        user_id: teacher.id,
-      });
-      Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
-    }*/
-  
-    async function handleToggleFavorite() {
-      const favorites = await AsyncStorage.getItem('favorites');
-      let favoritesArray = [];
-  
-      if (favorites) {
-        favoritesArray = JSON.parse(favorites);
-      }
-  
-      if (isFavorited) {
-        const favoriteIndex = favoritesArray.findIndex((casosItem) => {
-          return casosItem.id === casos.id;
-        });
-        favoritesArray.splice(favoriteIndex, 1);
-        setIsFavorited(false);
-      } else {
-        favoritesArray.push(casos);
-        setIsFavorited(true);
-      }
-  
-      await AsyncStorage.setItem('favorites', JSON.stringify(favoritesArray));
+export default function CasosItem({ casos, favorited }: TCaseProps) {
+  const navigation = useNavigation()
+  const [isFavorited, setIsFavorited] = useState(favorited);
+
+  async function handleToggleFavorite() {
+    const favorites = await AsyncStorage.getItem("favorites");
+    let favoritesArray = [];
+
+    if (favorites) {
+      favoritesArray = JSON.parse(favorites);
     }
-  
-    return (
+
+    if (isFavorited) {
+      const favoriteIndex = favoritesArray.findIndex((casosItem: TCases) => {
+        return casosItem.id === casos.id;
+      });
+      favoritesArray.splice(favoriteIndex, 1);
+      setIsFavorited(false);
+    } else {
+      favoritesArray.push(casos);
+      setIsFavorited(true);
+    }
+
+    await AsyncStorage.setItem("favorites", JSON.stringify(favoritesArray));
+  }
+
+  return (
     <View style={styles.container}>
-    <View style={styles.profile}>
-      <Image
-        style={styles.avatar}
-        source={{ uri: casos.avatar }}
-      />
-
-      <View style={styles.profileInfo}>
-        <Text style={styles.name}>{casos.name}</Text>
-        <Text style={styles.subject}>{casos.subject}</Text>
+      <View style={styles.profile}>
+        <Image style={styles.avatar} source={{ uri: casos.avatar }} />
       </View>
-    </View>
-    <Text style={styles.bio}>
-      {casos.bio}
-    </Text>
+      <Text style={styles.name}>{casos.name}</Text>
+      <Text style={styles.subject}>{casos.subject}</Text>
+      <Text style={styles.bio}>{casos.bio}</Text>
 
-    <View style={styles.footer}>
-      <Text style={styles.price}>
-        Preço {'   '}
-        <Text style={styles.priceValue}>R$ {casos.cost}</Text>
-      </Text>
-
-      <View style={styles.buttonsContainer}>
+      <View style={styles.footer}>
         <RectButton
-          style={[
-            styles.favoriteButton,
-            isFavorited ? styles.favorited : {}
-          ]}
+          style={[styles.favoriteButton, isFavorited ? styles.favorited : {}]}
           onPress={handleToggleFavorite}
         >
-          {isFavorited
-            ? <Image source={unfavoriteIcon} />
-            : <Image source={heartOutlineIcon} />
-          }
+          {isFavorited ? (
+            <Image source={unfavoriteIcon} />
+          ) : (
+            <Image source={heartOutlineIcon} />
+          )}
         </RectButton>
 
-        <RectButton  style={styles.contactButton}>
+        <RectButton style={styles.contactButton} onPress={() => navigation.navigate('CompleteRegistration')}>
           <Image source={whatsappIcon} />
-          <Text style={styles.contactButtonText}>Entrar em contato</Text>
         </RectButton>
       </View>
     </View>
-  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderWidth: 1,
-    borderColor: '#e6e6f0',
+    borderColor: "#e6e6f0",
     borderRadius: 8,
     marginBottom: 16,
-    overflow: 'hidden',
+    overflow: "hidden",
+    padding: 24,
+    elevation: 4,
   },
 
   profile: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 24,
+    flexDirection: "row",
+    alignItems: "center",
   },
 
   avatar: {
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    backgroundColor: '#eee',
-  },
-
-  profileInfo: {
-    marginLeft: 16,
+    width: 300,
+    height: 182,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    marginBottom: 10,
+    backgroundColor: "#eee",
   },
 
   name: {
-    fontFamily: 'Archivo_700Bold',
-    color: '#32264d',
+    color: "#000",
     fontSize: 20,
   },
 
   subject: {
-    fontFamily: 'Poppins_400Regular',
-    color: '#6a6180',
-    fontSize: 12,
-    marginTop: 4,
-  },
-
-  bio: {
-    marginHorizontal: 24,
-    fontFamily: 'Poppins_400Regular',
+    color: "#6a6180",
     fontSize: 14,
-    lineHeight: 24,
-    color: '#646180'
+    marginTop: 2,
+  },
+  
+  bio: {
+    marginTop: 10,
+    fontSize: 12,
+    color: "#000",
   },
 
   footer: {
-    color: '#fafafc',
-    padding: 24,
-    alignItems: 'center',
+    color: "#fafafc",
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
     marginTop: 24,
-  },
-
-  price: {
-    color: '#6a6180',
-    fontSize: 14,
-  },
-
-  priceValue: {
-    color: '#F5B234',
-    fontSize: 16,
-  },
-
-  buttonsContainer: {
-    flexDirection: 'row',
-    marginTop: 16,
+    flexDirection: "row",
   },
 
   favoriteButton: {
-    backgroundColor: '#F5B234',
+    backgroundColor: "#F5B234",
     width: 56,
     height: 56,
     borderRadius: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginRight: 8,
   },
 
   favorited: {
-    backgroundColor: '#e33d3d',
+    backgroundColor: "#e33d3d",
   },
 
   contactButton: {
-    backgroundColor: '#04d361',
-    flex: 1,
+    backgroundColor: "#04d361",
+    width: 56,
     height: 56,
-    flexDirection: 'row',
+    flexDirection: "row",
     borderRadius: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginRight: 8,
   },
 
   contactButtonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
     marginLeft: 16,
   },
