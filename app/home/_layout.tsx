d13f2acb8b427e57723ccc8f2e74657223336cee
oopaze <pedroosd28@gmail.com>
2023-10-24 01:17:06 -0300

    feat: adding tabs conf

---
+++ app/home/_layout.tsx
@@ -0,0 +1,53 @@
+import { USER_BASE_FIELDS_KEY } from "@/components/login-form";
+import useCache from "@/hooks/useCache";
+import { IUserType } from "@/services/user/create";
+import { Tabs } from "expo-router";
+
+const DEFAULT_TABS_CONF = {
+  headerShown: false,
+};
+
+const Layout = () => {
+  const { cachedValue } = useCache<{ type: IUserType }>(USER_BASE_FIELDS_KEY);
+
+  const isOng = cachedValue?.type === "ong";
+  const isVoluntary = cachedValue?.type === "voluntary";
+
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="events"
+        options={{
+          ...DEFAULT_TABS_CONF,
+          href: "/home/events",
+        }}
+      />
+      <Tabs.Screen
+        name="subscribed-events"
+        options={{
+          ...DEFAULT_TABS_CONF,
+          href: "/home/subscribed-events",
+          tabBarItemStyle: {
+            display: isVoluntary ? "flex" : "none",
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="my-events"
+        options={{
+          ...DEFAULT_TABS_CONF,
+          href: "/home/my-events",
+          tabBarItemStyle: {
+            display: isOng ? "flex" : "none",
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{ ...DEFAULT_TABS_CONF, href: "/home/profile" }}
+      />
+    </Tabs>
+  );
+};
+
+export default Layout;
